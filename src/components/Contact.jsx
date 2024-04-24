d7b9885342a1bf150248d1d21adf927277e688dc
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-04-24 11:51:35 +0530

    Refactor form submission logic in Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,22 +2,33 @@
 import gmailGif from "../Assets/Email.gif";
 import emailjs from "@emailjs/browser"
 import { useState } from 'react';
-import { useForm } from "react-hook-form"
 
 const Contact = () => {
-  const { register, handleSubmit , reset} = useForm()
-  const [errors, setError] = useState('')
   const scrollPageUp = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
   
-  const contact = async (data) => {
-    setError('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       const result = await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        data,
+        formData,
         {
           publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
         }
@@ -26,22 +37,39 @@ const Contact = () => {
       console.log(result.text);
       if(result){
         alert('Message sent successfully');
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
         scrollPageUp();
-        reset(); // Reset the form after successful submission
       }
     } catch (error) {
       console.error(error);
-      setError(error.message || "Message not sent");
-      alert(errors.message || "Message not sent")
-      reset()
+      alert(error.message || "Message not sent")
+      setFormData({
+        name: '',
+        email: '',
+        message: '',
+      });
       scrollPageUp()
     }
   };
   return (
-    <div className='flex flex-row items-center justify-center'>
-      <form
-            onSubmit={handleSubmit(contact)}
-            className="contact flex flex-col gap-5 w-fit items-center justify-center"
+    <>
+      <div className="text-center mb-14">
+        <button className="text-center text-2xl  btn btn-sm dark:btn-outline btn-outline bg-gray-700">
+          Let&lsquo;s connect
+        </button>
+      </div>
+      <section className="contact-section dark:bg-slate-800 bg-gray-200 w-full flex flex-col lg:flex-row items-center justify-evenly">
+        <div className="hidden lg:flex">
+          <img src={gmailGif} alt="Email" />
+        </div>
+        <div className="contact-form flex flex-col justify-center items-center border border-gray-500 p-7 rounded-xl w-fit lg:w-5/12">
+          <form
+            onSubmit={handleSubmit}
+            className="contact flex flex-col gap-5 w-full"
           >
             <div className="flex flex-row gap-1">
               <label className="input input-bordered flex items-center gap-2 w-full">
@@ -54,9 +82,8 @@ const Contact = () => {
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                 </svg>
                 <input
-                  {...register("username", {
-                    required: true,
-                  })}
+                value={formData.name}
+                  onChange={handleChange}
                   type="text"
                   className="grow"
                   placeholder="Username"
@@ -75,22 +102,20 @@ const Contact = () => {
                   <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                 </svg>
                 <input
+                  value={formData.email}
                   type="text"
                   className="grow"
                   placeholder="Email"
-                  {...register("email", {
-                    required: true
-                  })}
+                  onChange={handleChange}
                 />
               </label>
             </div>
             <div className="w-full flex flex-row gap-1">
               <textarea
+                value={formData.message}
                 className="textarea textarea-bordered h-20 w-full"
                 placeholder="Message"
-                {...register("message", {
-                  required: true,
-                })}
+                onChange={handleChange}
                 minLength={10}
               ></textarea>
             </div>
@@ -101,7 +126,9 @@ const Contact = () => {
               Send Message
             </button>
           </form>
-    </div>
+        </div>
+      </section>
+    </>
   );
 };
 
